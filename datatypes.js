eebcbd3e0f0038dd39a8b9f36406189064dd5dbc
peter kariri <peternjugunaw25@gmail.com>
2025-01-27 22:08:00 +0300

    javascriptintro

---
+++ datatypes.js
@@ -0,0 +1,78 @@
+/* data types are convectional way of stroing data in javascript 
+there are dfferent types of data types in javascritp
+they refere to the doifferet types of data they store
+they are divided into two{
+    primitive datatypes and complex data types
+
+    primitive data types::are datattypes that cannot be changed in that they allow only oine set of data type
+    they include {
+        strings{
+            they store any texual content in a web browser
+            they are defined and kept in quotes{
+                can be single or double quotes
+                string literal>used the backticks and the dollar sign
+
+                let school="eldohub"
+                let student_name='winnie'
+                let both=`${school}${student_name}`
+            }
+        }
+        Number{
+            any numerical data{
+                var myNum=34
+            }
+
+        }
+        Boolean{
+            this referes to a condition that can i=either be true or false 
+            1 or 0
+
+            genderIsFemale=true
+            
+            genderIsMale=true
+            let rain=false
+        }
+        null{
+            intentional abscence of value or not applicanble
+            let salary=null;
+        }
+        undefined{
+            a variable declared and no value has been issued or value been called but it is unrecognised 
+            normal declaration{
+                let location="kisumu"
+            }
+            the undefined one
+            let location;
+        }
+    }
+    complex data types{
+        data inside can be changed 
+
+
+        they include 
+        arrays  
+        objects
+        json
+    }
+} */
+
+    let name="Stephen Samlinks"
+    let sName="winnie"
+    let fullClass=name+' ' +sName
+    let fullName=`${name} ${sName}`
+    let myAge=45
+        let isScholar=true;
+        let freeTime=null;
+
+     
+
+        console.log(name);
+        console.log(myAge);
+        console.log(isScholar);
+        console.log(freeTime);
+        console.log(fullClass);
+        console.log(fullName);
+        
+        
+       
+        
---
+++ variables.js
@@ -0,0 +1,38 @@
+/* variables>>theree are three unique namiong varibles used in javascript 
+they are storage components that are used to store assigned values 
+they include {
+    var>>older variabe declaration,they can be redeclared in a code base under function scope
+
+    let>>newest modern variable declaratrion that has block scope reassignment 
+
+   
+
+    const>elements and values do not change after assignment >>cannot be redeclared
+
+       naming conventions variable name =value
+} */
+
+       var myName="Peter"
+
+       let age=345
+
+       const PI=3.14
+
+       console.log(myName);
+       console.log(age);
+       console.log(PI);
+
+       myName="stephen"
+
+       age=456
+
+       
+
+       console.log(myName);
+       console.log(age);
+       console.log(PI);
+
+
+
+       
+
